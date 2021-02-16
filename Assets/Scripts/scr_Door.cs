20afb4f77f5ee2eb56a218df67bd50696e10c3d2
Jimboz28 <70271665+Jimboz28@users.noreply.github.com>
2021-02-15 21:36:59 -0500

    added gameplay elements
    added gameplay elements

---
+++ Assets/Scripts/scr_Door.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr_Door : MonoBehaviour
+{
+    public float rotation;
+    public void OpenDoor()
+    {
+        transform.Rotate(0.0f, rotation,0.0f, Space.Self);
+    }
+}
---
+++ Assets/Scripts/scr_Mplatform.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr_Mplatform : MonoBehaviour
+{
+// Transforms to act as start and end markers for the journey.
+public Transform startMarker;
+public Transform endMarker;
+public float startTime;
+public float speed;
+public float sDistance;
+public float eDistance;
+
+private Vector3 currentPos;
+private Rigidbody rb;
+
+CharacterController cc;
+
+
+void Start()
+     {
+          rb = GetComponent<Rigidbody>();
+     }
+
+// Follows the target position like with a spring
+void FixedUpdate()
+     {
+     currentPos = Vector3.Lerp(startMarker.position, endMarker.position, Mathf.Cos(Time.time / startTime * Mathf.PI * speed) * sDistance * eDistance);
+     rb.MovePosition(currentPos);
+     }
+     private void OnTriggerEnter (Collider other)
+     {
+         if (other.tag == "Player")
+         {
+             cc = other.GetComponent<CharacterController>();
+         }
+     }
+     private void OnTriggerStay (Collider other)
+     {
+         if (other.tag == "Player")
+         {
+             cc.Move(rb.velocity * Time.deltaTime);
+         }
+     }
+}
