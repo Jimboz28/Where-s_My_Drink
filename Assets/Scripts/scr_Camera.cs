f1cf266963f32b686b71dbb9e56cfa3ae7d44009
Jimboz28 <70271665+Jimboz28@users.noreply.github.com>
2021-02-16 17:11:47 -0500

    navmesh working and some controller input

--- Assets/Scripts/scr_Camera.cs
+++ Assets/Scripts/scr_Camera.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class scr_Camera : MonoBehaviour
 {
     public float mouseSensitivity = 100f;
+    public float joySensitivity = 100f;
 
     public Transform playerBody;
 
     float xRotation = 0f;
     float yRotation = 0f;
+
     // Start is called before the first frame update
     
     void Start()
@@ -20,11 +22,19 @@ public class scr_Camera : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+       
        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+       
+       
+       float joystickX = Input.GetAxis ("Joystick X") * joySensitivity * Time.deltaTime;
+       float joystickY = Input.GetAxis("Joystick Y") * joySensitivity * Time.deltaTime;
 
        xRotation -= mouseY;
        yRotation += mouseX;
+       
+       xRotation -= joystickY;
+       yRotation += joystickX;
 
        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
---
+++ Assets/Scripts/scr_Enemy.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class scr_Enemy : MonoBehaviour
+{
+    public Transform [] points;
+    public int destPoint = 0;
+    public NavMeshAgent agent;
+    
+    void Start()
+    {
+        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+        agent.autoBraking = false;
+        GotoNextPoint();
+    }
+    void GotoNextPoint()
+    {
+        if (points.Length == 0)
+        return;
+        agent.destination = points[destPoint].position;
+        destPoint = (destPoint + 1) % points.Length;
+    }
+
+    void Update()
+    {
+        if (!agent.pathPending && agent.remainingDistance < 0.5f)
+        GotoNextPoint();
+    }
+
+}
